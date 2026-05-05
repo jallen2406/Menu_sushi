953a829f94d1a80127b667af19ff6362bab26579
jallen2406 <jallen.f24@gmail.com>
2026-05-04 20:02:01 -0400

    Remove almacen folder from tracking and add to .gitignore

--- almacen/Aprendizaje EA1 (PYTHON)/ejercicio5.py
+++
@@ -1,9 +0,0 @@
-promedio=float(input("Ingrese su promedio(1.0/7.0): "))
-quintil=int(input("Ingrese su quintil(1/5): "))
-
-if promedio>6.5 and quintil<=2:
-    print("Usted obtiene un computador")
-elif promedio>6.5 and 3<=quintil<=5:
-    print("Usted recibe una tablet")
-elif promedio<=6.5:
-    print("Recibe un diploma")
